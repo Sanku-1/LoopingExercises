e8602dccfcfcf09beb6d06a989f6c3d6cc6c6310
Stephen_A <ankustephen99@gmail.com>
2025-10-02 11:44:50 -0400

    Changed loop other parameter in ForLoop.java from i++ to i += 1 (aesthetic)

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 11; i += 1) {
             Thread.sleep(1000);
             if (i < 10) {
                 System.out.println(10 - i);
