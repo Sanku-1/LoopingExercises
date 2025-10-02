53f7555fefac4f1601c0ebdad87b71b3e10df69d
Stephen_A <ankustephen99@gmail.com>
2025-10-02 11:41:21 -0400

    Added 1000ms pause in for loop countdown

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,8 +1,9 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 11; i++) {
+            Thread.sleep(1000);
             if (i < 10) {
                 System.out.println(10 - i);
             } else {
